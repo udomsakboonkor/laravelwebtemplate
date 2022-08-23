c697c84d98f3427ac73266aee3187b3b90bc91d3
Udomsak Boonkor <udomsak@nfe.go.th>
2022-08-23 11:52:58 +0700

    first commit 2

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('about', [HomeController::class,'about']);
 Route::get('service', [HomeController::class,'service']);
 Route::get('contact', [HomeController::class,'contact']);
 Route::get('login', [HomeController::class,'login']);
+
+Route::get('product',function(){
+    return "Product Page";
+});
